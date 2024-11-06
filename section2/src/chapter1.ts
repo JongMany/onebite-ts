212902aef56936ff7154a62a70f6af6b4070825d
blackberry1114 <blackberry1114@adenasoft.com>
2024-11-07 08:24:34 +0900

    기본자료형

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,31 @@
+// Number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+
+// string
+let str1: string = "hello"
+let str2: string = 'hello';
+
+// boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined
+let und1: undefined = undefined;
+
+// Literal
+let numA: 10 = 10;
+// numA = 12; // Error
+
+let strA: 'hello' = 'hello';
+
+let boolA: true = true;
