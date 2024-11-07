0c4b9900d04824e8d500c4ad2c7c5fec5a862bd0
blackberry1114 <blackberry1114@adenasoft.com>
2024-11-08 08:11:28 +0900

    Type Alias, Index Signature

---
+++ section2/src/chapter4.ts
@@ -0,0 +1,55 @@
+// 타입 별칭
+type User = {
+  id: number;
+  name: string;
+  nickname: string;
+  birth: string;
+  bio: string;
+  location: string;
+};
+
+let user: User = {
+  id: 1,
+  name: "이정환",
+  nickname: "winterlood",
+  birth: "1997.01.07",
+  bio: "안녕하세요",
+  location: "부천시",
+};
+
+// Index Signature
+type CountryCodes = {
+  [key: string]: string;
+};
+
+let countryCodes: CountryCodes = {
+  Korea: "ko",
+  UnitedState: "us",
+  UnitedKingdom: "uk",
+};
+
+type CountryNumberCodes = {
+  [key: string]: number;
+};
+
+let countryNumberCodes: CountryNumberCodes = {
+  Korea: 410,
+  UnitedState: 840,
+  UnitedKingdom: 826,
+};
+
+{
+  type CountryCodesExceptKorea =
+    | {
+        [key: string]: string;
+      }
+    | {
+        Korea: number;
+      };
+
+  let countryCodes: CountryCodesExceptKorea = {
+    Korea: 123,
+    UnitedState: "us",
+    UnitedKingdom: "uk",
+  };
+}
